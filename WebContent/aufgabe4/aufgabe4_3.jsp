c656fa029b00de0f6ec82251f0b1941d948f9814
ArmerTropf <mguenster@web.de>
2017-04-03 17:05:45 +0200

    4.3 fertig; kleine aenderungen an title

--- WebContent/aufgabe4/aufgabe4_3.jsp
+++ WebContent/aufgabe4/aufgabe4_3.jsp
@@ -3,13 +3,13 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Aufgabe 4.2</title>
+<title>Aufgabe 4.3</title>
 </head>
 <body>
 <H1>MyProperties</H1>
 	<table>
-		<jsp:useBean id="obj" class="aufgabe4.Aufgabe4_2" />
-		<c:forEach var="item" items="${obj.myProperties}">
+		<jsp:useBean id="obj" class="aufgabe4.Aufgabe4_3" />
+		<c:forEach var="item" items="${obj.names }">
 			<tr>
 				<td>${item.key}</td>
 				<td>${item.value}</td>
---
+++ src/aufgabe4/Aufgabe4_3.java
@@ -0,0 +1,64 @@
+package aufgabe4;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Aufgabe4_3 {
+
+	private String XML = "C:/Users/ArmerTropf/Documents/GitHub/webprog2/WebContent/aufgabe4/persons.xml";
+	private Map<String, String> names = new HashMap<String, String>();
+
+	public Aufgabe4_3() {
+		try {
+
+			File fXmlFile = new File(XML);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			doc.getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("person");
+
+			String vorname = "";
+			String nachname = "";
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+					vorname = eElement.getElementsByTagName("firstname").item(0).getTextContent();
+					if (eElement.getElementsByTagName("lastname").item(0) != null) {
+						nachname = eElement.getElementsByTagName("lastname").item(0).getTextContent();
+					} else
+						nachname = "*";
+
+				}
+				names.put(vorname, nachname);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Map<String, String> getNames() {
+		return names;
+	}
+
+	public void setNames(Map<String, String> names) {
+		this.names = names;
+	}
+
+}
